6fa32d4f0468aca0bbe6e175911db97736ac0023
SergSentry <rtfm.faq@mail.ru>
2023-05-25 19:59:17 +0300

    base algorithm: add task_10

---
+++ 02_lesson_base_algorithm/task_10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+while (true)
+{
+    Console.Write("Enter a three digit number: ");
+    var digit_str = Console.ReadLine();
+
+    int digit;
+    if (int.TryParse(digit_str, out digit))
+    {
+        if (digit > 0 && digit_str.Length == 3)
+            Console.Write(digit_str[1]);
+        else if (digit < 0 && digit_str.Length == 4)
+            Console.Write(digit_str[2]);
+
+        break;
+    }
+}
